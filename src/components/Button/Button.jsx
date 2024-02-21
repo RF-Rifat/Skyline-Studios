1394585791d2ddb752721c2a3ef56a8c8887ffc4
RF-Rifat <rfrifat6344@gmail.com>
2024-02-21 10:09:32 +0600

    custom button created

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import { Link } from "react-router-dom";
 const Button = ({ text, color }) => {
   return (
@@ -7,7 +8,9 @@ const Button = ({ text, color }) => {
         className="relative inline-flex items-center justify-start py-3 pl-4 pr-14 overflow-hidden font-semibold transition-all duration-500 ease-in-out rounded hover:pl-14 hover:pr-6 group"
       >
         <span className="absolute right-0 pr-4 duration-200 ease-out group-hover:">
-          <div className="bg-[#FFFF00] p-2 rounded-full group-hover:scale-0 duration-200 ease-out">
+          <div
+            className={`${color} p-2 rounded-full group-hover:scale-0 duration-200 ease-out`}
+          >
             <svg
               className="w-6 h-6 -rotate-45"
               fill="none"
@@ -25,7 +28,7 @@ const Button = ({ text, color }) => {
           </div>
         </span>
         <span className="absolute left-4 -transl group-hover:translate-x-0 ease-out duration-200">
-          <div className="bg-[#FFFF00] p-2 rounded-full">
+          <div className={`${color} p-2 rounded-full`}>
             <svg
               className="w-6 h-6 -rotate-45"
               fill="none"
@@ -42,7 +45,9 @@ const Button = ({ text, color }) => {
             </svg>
           </div>
         </span>
-        <span className="relative w-full text-left transition-colors duration-200 ease-in-out  bg-[#FFFF00] rounded-full px-4 py-2 text-xl">
+        <span
+          className={`${color} relative w-full text-left transition-colors duration-200 ease-in-out  rounded-full px-4 py-2 text-lg`}
+        >
           {text}
         </span>
       </Link>
