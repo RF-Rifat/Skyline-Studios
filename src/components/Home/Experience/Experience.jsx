87b40d2e7e3c3d6b94db795e202d01228ea5d2d4
RF-Rifat <rfrifat6344@gmail.com>
2024-02-22 14:54:59 +0600

    video animation created

--- src/components/Home/Experience/Experience.jsx
+++ src/components/Home/Experience/Experience.jsx
@@ -9,7 +9,7 @@ const Experience = () => {
   const controls = useAnimation();
   const textControls = useAnimation();
   const { scrollY } = useViewportScroll();
-
+  const { x, y } = useFollowPointer(ref);
   const [isHovering, setIsHovering] = useState(false);
   const [isScrolled, setIsScrolled] = useState(false);
 
@@ -35,16 +35,15 @@ const Experience = () => {
     controls.start({ scale: scale });
     setIsScrolled(latest > 0);
   });
-  scrollY.onChange((latest) => {
-    const size = Math.max(1, 1 + latest / 1000);
-    console.log(latest);
-    textControls.start({ scale: size });
-    setIsScrolled(latest > 0);
-  });
+   scrollY.onChange((latest) => {
+     const size = Math.max(1, 1 + latest / 1000);
+     textControls.start({ scale: size });
+     setIsScrolled(latest > 0);
+   });
 
   return (
     <div className="bg-white overflow-hidden relative">
-      <div className="text-center relative">
+      <div className="text-center">
         <motion.section
           style={{
             willChange: "transform, width, height",
@@ -69,46 +68,108 @@ const Experience = () => {
                 type="video/mp4"
               />
             </video>
-            {isHovering && (
-              <div className="absolute top-0 left-0">
-                <img
-                  src="https://example.com/mouse-image.png"
-                  alt="Mouse"
-                  className="mouse-image"
-                />
-              </div>
-            )}
+          </motion.div>
+
+          <motion.div
+            ref={ref}
+            className={!isHovering && "hidden"}
+            animate={{ x, y }}
+            transition={{
+              type: "spring",
+              damping: 3,
+              stiffness: 50,
+              restDelta: 0.001,
+            }}
+            style={{
+              position: "absolute",
+              width: "50px",
+              height: "50px",
+            }}
+          >
+            <div
+              className="relative h-32 w-32 spin"
+              onClick={handleVideoClick}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+            >
+              <img
+                src="https://assets-global.website-files.com/643f7373d3f6653157617339/6448e195835c273a8d71af34_Video%20Player%20Icon.svg"
+                alt="play now"
+                className="absolute inset-0 m-auto h-12 w-12"
+              />
+              <img
+                loading="lazy"
+                alt="play now"
+                src="https://assets-global.website-files.com/643f7373d3f6653157617339/648050ec6410b577150dc4cf_video%20text%20circle.svg"
+              />
+            </div>
           </motion.div>
         </motion.section>
-        <motion.div
-          className="absolute top-0"
-          animate={textControls}
-          data-aos="fade-down"
-        >
-          Elevating UX, Empowering
-        </motion.div>
-        <motion.div
-          className="absolute bottom-0 text-center"
-          animate={textControls}
-          data-aos="fade-down"
-        >
-          Experiences...
-        </motion.div>
-        <motion.div
-          className="absolute left-0 translate-x-1/2"
-          animate={textControls}
-          data-aos="fade-down"
-        >
-          Businesses,
-        </motion.div>
-        <motion.div
-          className="absolute right-0"
-          animate={textControls}
-          data-aos="fade-down"
-        >
-          Redefining
-        </motion.div>
       </div>
+      <motion.div
+        className="absolute top-0"
+        animate={textControls}
+        data-aos="fade-down"
+      >
+        Elevating UX, Empowering
+      </motion.div>
+      <motion.div
+        className="absolute bottom-0 text-center"
+        animate={textControls}
+        data-aos="fade-down"
+      >
+        Experiences...
+      </motion.div>
+      <motion.div
+        className="absolute left-0 translate-x-1/2"
+        animate={textControls}
+        data-aos="fade-down"
+      >
+        Businesses,
+      </motion.div>
+      <motion.div
+        className="absolute right-0"
+        animate={textControls}
+        data-aos="fade-down"
+      >
+        Redefining
+      </motion.div>
+      {/* {isScrolled && (
+        <div className="flex justify-between">
+          <motion.div
+            initial={{ scale: 0 }}
+            animate={{ scale: 1 }}
+            transition={{ duration: 0.5 }}
+            className="text-block"
+          >
+            Text 1
+          </motion.div>
+          <motion.div
+            initial={{ scale: 0 }}
+            animate={{ scale: 1 }}
+            transition={{ duration: 0.5, delay: 0.1 }}
+            className="text-block"
+          >
+            Text 2
+          </motion.div>
+          <motion.div
+            initial={{ scale: 0 }}
+            animate={{ scale: 1 }}
+            transition={{ duration: 0.5, delay: 0.2 }}
+            className="text-block"
+          >
+            Text 3
+          </motion.div>
+          <motion.div
+            initial={{ scale: 0 }}
+            animate={{ scale: 1 }}
+            transition={{ duration: 0.5, delay: 0.3 }}
+            className="text-block"
+          >
+            Text 4
+          </motion.div>
+        </div>
+      )} */}
       <ExDesc />
     </div>
   );
