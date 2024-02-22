4dd9197d6ece6cba8f98be7c93fdd67e406543a1
RF-Rifat <rfrifat6344@gmail.com>
2024-02-22 12:57:20 +0600

    video animation created

--- src/components/Home/Experience/ExDesc.jsx
+++ src/components/Home/Experience/ExDesc.jsx
@@ -1,13 +1,11 @@
+import Button from "../../shared/Button/Button";
+
 const ExDesc = () => {
   return (
-    <div className="container">
-      <div
-        id="w-node-_68f647ab-9fb1-564a-f805-2a71ef120da0-c451ecec"
-        className="who-section-content-wrap"
-      >
+    <div className="container" data-aos="fade-up">
+      <div className="who-section-content-wrap">
         <div className="section-cotent-wrap">
           <p
-            data-w-id="6a440699-64fd-3d20-6bea-d54177c53dcf"
             style={{
               opacity: 1,
               transform:
@@ -38,59 +36,11 @@ const ExDesc = () => {
             UX&quot; is more than just a tagline; it&apos;s our commitment to
             you.
           </p>
-          <div
-            data-w-id="105a8e57-9874-7f3b-2d43-7005658294e6"
-            style={{
-              opacity: 1,
-              transform:
-                "translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)",
-              transformStyle: "preserve-3d",
-            }}
-            className="btn-wrapper"
-          >
-            <div
-              data-w-id="624eb6a2-1418-2674-a9b4-1b01f305e444"
-              className="button-hover-wrap"
-            >
-              <div className="button-icon left main-bg-color">
-                <img
-                  alt=""
-                  loading="lazy"
-                  src="https://assets-global.website-files.com/643f7373d3f6653157617339/6475e07a5346abd5e16df149_short_down.svg"
-                  className="image-icon"
-                />
-              </div>
-              <a
-                rel="nofollow"
-                href="/contact"
-                className="primary-button-2 main-bg-color w-inline-block"
-                style={{
-                  transform:
-                    "translate3d(-58px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)",
-                  transformStyle: "preserve-3d",
-                }}
-              >
-                <div className="button-content">
-                  <div className="button-color color-black">Contact us</div>
-                </div>
-              </a>
-              <div
-                className="button-icon right main-bg-color"
-                style={{
-                  transform:
-                    "translate3d(-58px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)",
-                  transformStyle: "preserve-3d",
-                }}
-              >
-                <img
-                  alt="short down"
-                  loading="lazy"
-                  src="https://assets-global.website-files.com/643f7373d3f6653157617339/6475e07a5346abd5e16df149_short_down.svg"
-                  className="image-icon"
-                />
-              </div>
-            </div>
-          </div>
+          <Button
+            text={"Contact Us"}
+            color={"bg-[#FFFF00]"}
+            fontClr={"text-[#000000]"}
+          />
         </div>
       </div>
     </div>
--- src/components/Home/Experience/Experience.jsx
+++ src/components/Home/Experience/Experience.jsx
@@ -1,95 +1,115 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { motion, useAnimation, useViewportScroll } from "framer-motion";
 import "./Spin.css";
 import { useFollowPointer } from "./useFollowPointer";
+import ExDesc from "./ExDesc";
 
 const Experience = () => {
-  const videoRef = useRef(null);
+  // const videoRef = useRef(null);
   const ref = useRef(null);
   const controls = useAnimation();
   const { scrollY } = useViewportScroll();
   const { x, y } = useFollowPointer(ref);
-
+  const [isHovering, setIsHovering] = useState(false);
   const handleVideoClick = () => {
-    const video = videoRef.current;
-    if (video.paused) {
-      video.play();
-    } else {
+    const video = ref.current;
+    if (video?.paused) {
       video.pause();
+    } else if (video) {
+      video.play();
     }
+  };
+
+  const handleMouseEnter = () => {
+    setIsHovering(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovering(false);
   };
 
   scrollY.onChange((latest) => {
     const scale = Math.max(0.1, 1 - latest / 1000);
     controls.start({ scale: scale });
   });
-
   return (
-    <div className="bg-white">
-      <div className="showreel-sticky">
-        <div className="about-title-content text-center">
-          <motion.section
-            style={{
-              willChange: "transform, width, height",
-              transform:
-                "translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)",
-              width: "100%",
-              height: "100%",
-            }}
-          >
-            <motion.div
-              ref={videoRef}
-              animate={controls}
-              className="video-embed w-embed"
-            >
-              <video
-                className="wb-video-styles-controller"
-                poster="https://uiart.io/video/MM-reel.gif"
-                loop
-                onClick={handleVideoClick}
-              >
-                <source
-                  src="https://uiart.io/video/main_showreel.mp4"
-                  type="video/mp4"
-                />
-              </video>
-            </motion.div>
-
-            <motion.div
-              ref={ref}
-              className="box"
-              animate={{ x, y }}
-              transition={{
-                type: "spring",
-                damping: 3,
-                stiffness: 50,
-                restDelta: 0.001,
-              }}
+    <>
+      <div className="bg-white overflow-hidden">
+        <div>
+          <div className="text-center">
+            <motion.section
               style={{
-                position: "absolute",
+                willChange: "transform, width, height",
+                transform:
+                  "translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)",
               }}
             >
-              <div
-                className="relative h-32 w-32 spin"
-                onClick={handleVideoClick}
+              <motion.div
+                animate={controls}
+                className="video-embed w-embed"
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
               >
-                <img
-                  src="https://assets-global.website-files.com/643f7373d3f6653157617339/6448e195835c273a8d71af34_Video%20Player%20Icon.svg"
-                  alt="play now"
-                  className="absolute inset-0 m-auto h-12 w-12"
-                />
-                <img
-                  loading="lazy"
-                  alt="play now"
-                  src="https://assets-global.website-files.com/643f7373d3f6653157617339/648050ec6410b577150dc4cf_video%20text%20circle.svg"
-                  className=""
-                />
-              </div>
-            </motion.div>
-          </motion.section>
+                <video
+                  ref={ref}
+                  poster="https://uiart.io/video/MM-reel.gif"
+                  loop
+                  onClick={handleVideoClick}
+                >
+                  <source
+                    src="https://uiart.io/video/main_showreel.mp4"
+                    type="video/mp4"
+                  />
+                </video>
+                {isHovering && (
+                  <div className={isHovering && "hidden"}>
+                    <img
+                      src="https://example.com/mouse-image.png"
+                      alt="Mouse"
+                      className="mouse-image"
+                    />
+                  </div>
+                )}
+              </motion.div>
+
+              <motion.div
+                ref={ref}
+                className="box"
+                animate={{ x, y }}
+                transition={{
+                  type: "spring",
+                  damping: 3,
+                  stiffness: 50,
+                  restDelta: 0.001,
+                }}
+                style={{
+                  position: "absolute",
+                  width: "50px",
+                  height: "50px",
+                }}
+              >
+                <div
+                  className="relative h-32 w-32 spin"
+                  onClick={handleVideoClick}
+                >
+                  <img
+                    src="https://assets-global.website-files.com/643f7373d3f6653157617339/6448e195835c273a8d71af34_Video%20Player%20Icon.svg"
+                    alt="play now"
+                    className="absolute inset-0 m-auto h-12 w-12"
+                  />
+                  <img
+                    loading="lazy"
+                    alt="play now"
+                    src="https://assets-global.website-files.com/643f7373d3f6653157617339/648050ec6410b577150dc4cf_video%20text%20circle.svg"
+                  />
+                </div>
+              </motion.div>
+            </motion.section>
+          </div>
         </div>
+        <ExDesc />
       </div>
-    </div>
+    </>
   );
 };
 
