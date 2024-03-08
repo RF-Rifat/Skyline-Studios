2722fcb24ed7555df4602b22345431266cdac6d2
RF-Rifat <rfrifat6344@gmail.com>
2024-03-08 16:03:54 +0600

    making all route responsive

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,9 +1,11 @@
 const AboutUs = () => {
   return (
-      <>
-          
-      </>
-  )
-}
+    <>
+      <div className="tracking-tight leading-snug font-bold text-5xl md:text-6xl xl:text-7xl">
+        Fueling Minds <br /> —Inspiring Designs..
+      </div>
+    </>
+  );
+};
 
-export default AboutUs
+export default AboutUs;
